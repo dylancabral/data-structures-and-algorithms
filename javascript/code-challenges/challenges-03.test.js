a318bda1589df94dc21f31a27889a4cc66ca0282
dylan <dylancabral.93@gmail.com>
2022-12-01 13:21:54 -0800

    code challenge 3 was unfinished

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -8,6 +8,11 @@ Write a function called addTwo that takes in an array and adds two to every valu
 
 const addTwo = (arr) => {
   // Solution code here...
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] + 2);
+  }
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -20,6 +25,7 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 
 const typeNum = (arr) => {
   // Solution code here...
+  return arr.filter(val => !isNaN(val));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -32,6 +38,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
+  return arr.filter(val => val.includes('and'));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -44,6 +51,7 @@ For example, oddValues([1,2,3]) returns [1,3].
 
 const oddValues = (arr) => {
   // Solution code here...
+  return arr.filter(val => (val % 2));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -56,8 +64,8 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
+  return arr.filter(val => !(forbiddenValues.includes(val)));
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6 - Stretch Goal
 
