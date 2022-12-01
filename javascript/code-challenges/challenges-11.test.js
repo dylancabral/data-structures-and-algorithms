ee3fe7541f7cfe4fb3f7f3f26b125a81e770c6e9
dylan <dylancabral.93@gmail.com>
2022-12-01 02:31:52 -0800

    cc 11

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -20,7 +20,8 @@ Becomes:
 
 function transformToLis(obj){
   // Solution code here...
-};
+  return Object.keys(obj).map(item => `<li>${item}: ${obj[item]}</li>`);
+}
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -33,7 +34,13 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // Solution code here...
+  let newArr = [];
+  for (let i in input) {
+    for (let j in input[i]) {
+      newArr.push(input[i][j]);
+    }
+  }
+  return newArr.reduce((total, item) => target === item ? total += 1 : total, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -47,7 +54,13 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  // Solution code here...
+  let newArr = [];
+  for (let i in input) {
+    for (let j in input[i]) {
+      newArr.push(input[i][j]);
+    }
+  }
+  return newArr.reduce((total, item) => total += item, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,7 +76,7 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map(arrs => arrs.filter(item => typeof item === 'number' && !(item % 5))).map(arr => arr.map(item => Math.pow(2, item)));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -129,8 +142,9 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
-  // Solution code here...
+  return data.filter(person => person.gender === 'male' || person.gender === 'female').reduce((str, person) => str === '' ? str = str + person.name : str = str + ` and ${person.name}`, '');
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
@@ -139,7 +153,7 @@ Write a function named findShortest that, given the Star Wars data from Challeng
 ------------------------------------------------------------------------------------------------ */
 
 let findShortest = (data) => {
-  // Solution code here...
+  return data.reduce((shortest, person) => parseInt(person.height) <= parseInt(shortest.height) ? {name: person.name, height: person.height} : {name: shortest.name, height: shortest.height}, {name: 'first', height: 300}).name;
 };
 
 /* ------------------------------------------------------------------------------------------------
